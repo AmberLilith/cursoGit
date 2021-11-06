1a61f941497323c16102b3a04b5f495a737541f2
Amber Lilith O. da Silva <amber.lilith.ordone@gmail.com>
2021-11-06 17:16:40 -0300

    Iniciando curso git

---
+++ src/main/java/com/br/projetoGit/projetoGit/ProjetoGitApplication.java
@@ -0,0 +1,13 @@
+package com.br.projetoGit.projetoGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/projetoGit/projetoGit/ProjetoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.projetoGit.projetoGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
